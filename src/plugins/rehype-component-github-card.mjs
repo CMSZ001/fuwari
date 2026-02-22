9d43c0a9bc4ae909d9404fa9511b77cc8085d7aa
CMSZ001 <130162476+CMSZ001@users.noreply.github.com>
2026-02-22 16:07:18 +0800

    Change GitHub API endpoint in rehype component

--- src/plugins/rehype-component-github-card.mjs
+++ src/plugins/rehype-component-github-card.mjs
@@ -58,7 +58,7 @@ export function GithubCardComponent(properties, children) {
 		`script#${cardUuid}-script`,
 		{ type: "text/javascript", defer: true },
 		`
-      fetch('https://gh.dpik.top/https://api.github.com/repos/${repo}', { referrerPolicy: "no-referrer" }).then(response => response.json()).then(data => {
+      fetch('https://api.github.cmliussss.com/repos/${repo}', { referrerPolicy: "no-referrer" }).then(response => response.json()).then(data => {
         document.getElementById('${cardUuid}-description').innerText = data.description?.replace(/:[a-zA-Z0-9_]+:/g, '') || "Description not set";
         document.getElementById('${cardUuid}-language').innerText = data.language;
         document.getElementById('${cardUuid}-forks').innerText = Intl.NumberFormat('en-us', { notation: "compact", maximumFractionDigits: 1 }).format(data.forks).replaceAll("\u202f", '');
