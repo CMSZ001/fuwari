c66189d414b6a0d10cfe9310980941b06165d711
CMSZ001 <130162476+CMSZ001@users.noreply.github.com>
2026-04-26 04:49:00 +0000

    feat: 更新 astro 配置以支持图像服务，移除补丁文件和相关依赖

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import swup from "@swup/astro";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components"; /* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
@@ -177,4 +177,7 @@ export default defineConfig({
 			},
 		},
 	},
+	image: {
+        service: passthroughImageService()
+    },
 });
